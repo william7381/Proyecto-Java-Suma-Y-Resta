2187528d4de8eeb8c31df2ae072df0724fc80bb5
DannyReyes <danny.loko.97@gmail.com>
2017-08-09 16:28:31 -0500

    se agrego la suma

--- logic/Operation.java
+++ logic/Operation.java
@@ -2,4 +2,7 @@ package logic;
 
 public class Operation {
 
+	public double sum(double num1, double num2){
+		return num1 + num2;
+	}
 }
--- run/Run.java
+++ run/Run.java
@@ -3,7 +3,8 @@ package run;
 public class Run {
 	
 	public static void main(String[] args) {
-		
+		Operation operation = new Operation();
+		System.out.println(operation.sum(1,2));
 	}
 
 }
